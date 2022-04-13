dfae9fe77d067f41aac74c1386e39f2162de2a1c
EduardoToledoMIAD <92895261+EduardoToledoMIAD@users.noreply.github.com>
2022-04-13 14:19:34 -0700

    Updating HTML and APP.PY

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ import pickle
 
 
 app= Flask(__name__, template_folder="templates")
+#model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = [x for x in request.form.values()]
+    print(features)
+    return render_template('index.html', id='predict', prediction_text='${}'.format(30))
+
 if __name__ == '__main__':
     app.run()
