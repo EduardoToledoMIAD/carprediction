e2674887dc56667761b72489c8249808c40ac4d3
EduardoToledoMIAD <92895261+EduardoToledoMIAD@users.noreply.github.com>
2022-04-13 15:06:08 -0700

    Updating PKL and app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 
 
 app= Flask(__name__, template_folder="templates")
-#model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def home():
@@ -15,7 +15,15 @@ def home():
 def predict():
     features = [x for x in request.form.values()]
     print(features)
-    return render_template('index.html', id='predict', prediction_text='${}'.format(30))
+    final_features = [np.array(features)]
+    column_names=['R&DSpend','Administration','MarketingSpend','State']
+    final_features=pd.DataFrame(final_features,columns=column_names)
+    prediction= model.predict(final_features)
+    temp=0.0
+    for i in prediction:
+        for j in i:
+            temp=j
+    return render_template('index.html', id='predict', prediction_text='${}'.format(temp))
 
 if __name__ == '__main__':
     app.run()
