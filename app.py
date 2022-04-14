02376d60b6ca272a55f7c8870591b546765cfdd6
EduardoToledoMIAD <92895261+EduardoToledoMIAD@users.noreply.github.com>
2022-04-14 14:26:15 -0700

    Updating app.py

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
+from apispec import APISpec
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
 from flask_restful import Resource, Api, reqparse
 import pickle
 
+class CarPrediction(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price1',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+        )
+        parser.add_argument('price2',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+        )
+        data = parser.parse_args()
+        item = {'name': 'Hola', 'price':10}
+        items.append(item)
+        return {'items': items},200
+
+
+
+
 
 app= Flask(__name__, template_folder="templates")
 # Definición API Flask
 api = Api(app)
 
-
 model = pickle.load(open('model.pkl', 'rb'))
 items = []
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -30,12 +52,10 @@ def predict():
             temp=j
     return render_template('index.html', id='predict', prediction_text='${}'.format(temp))
 
-class ItemList(Resource):
-    def get(self):
-        item = {'name': 'hola', 'price':10}
-        items.append(item)
-        return {'items': items},200
+@app.route('/api/doc',methods=['GET'])
+def api_documentation():
+     return render_template('documentation.html')
 
-api.add_resource(ItemList, '/api/items')
+api.add_resource(CarPrediction, '/api/car-prediction')
 if __name__ == '__main__':
     app.run()
