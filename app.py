43760f73edb30812e2649657cafd7cfbd3fa4e91
EduardoToledoMIAD <92895261+EduardoToledoMIAD@users.noreply.github.com>
2022-04-14 14:50:26 -0700

    Updating Files

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from apispec import APISpec
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
-from flask_restful import Resource, Api, reqparse
+from flask_restful import  reqparse
+from flask_restful_swagger_3 import Api,Resource,swagger, get_swagger_blueprint
 import pickle
 
 class CarPrediction(Resource):
@@ -29,7 +29,9 @@ class CarPrediction(Resource):
 
 app= Flask(__name__, template_folder="templates")
 # Definición API Flask
-api = Api(app)
+
+api = Api(app, version='5',  title="APP")
+
 
 model = pickle.load(open('model.pkl', 'rb'))
 items = []
@@ -52,10 +54,20 @@ def predict():
             temp=j
     return render_template('index.html', id='predict', prediction_text='${}'.format(temp))
 
-@app.route('/api/doc',methods=['GET'])
-def api_documentation():
-     return render_template('documentation.html')
+
+#@app.route('/api/doc',methods=['GET'])
+#def api_documentation():
+#     return render_template('documentation.html')
+
+SWAGGER_URL = '/api/doc'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'swagger.json'  # Our API url (can of course be a local resource)
+
+swagger_blueprint = get_swagger_blueprint(
+    api.open_api_object,
+    swagger_prefix_url=SWAGGER_URL,
+    swagger_url=API_URL)
 
 api.add_resource(CarPrediction, '/api/car-prediction')
+app.register_blueprint(swagger_blueprint)
 if __name__ == '__main__':
     app.run()
