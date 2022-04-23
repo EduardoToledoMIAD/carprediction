a80faee4f057565a55736d2aca4255dcb0a16339
EduardoToledoMIAD <92895261+EduardoToledoMIAD@users.noreply.github.com>
2022-04-22 17:20:50 -0700

    More Models

--- models/schemas.py
+++ models/schemas.py
@@ -1,11 +1,20 @@
 from marshmallow import Schema, fields, validate, ValidationError
 
-class CarPredictionSchema(Schema):
+class ProfitPredictionSchema(Schema):
     RDSpend = fields.Float(required=True)
     Administration = fields.Float(required=True)
     MarketingSpend = fields.Float(required=True)
     State = fields.Str(required=True)
-    
+
+class CarPredictionSchema(Schema):
+    Year = fields.Int(required=True)
+    Mileage= fields.Int(required=True)
+    State = fields.Str(required=True)
+    Make = fields.Str(required=True)
+    Model = fields.Str(required=True)
+
 class BatchSchema(Schema):
     batch = fields.List(fields.Nested(CarPredictionSchema))
 
+class Batch1Schema(Schema):
+    batch = fields.List(fields.Nested(ProfitPredictionSchema))
